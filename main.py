72eb7114cf0c719e02adf8a7ea5a1275b31af0ac
Brian Burns <bdburns6389@gmail.com>
2017-12-03 20:13:47 -0600

    changed deleting to Boolean hiding

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
 
     def __init__(self,name):
         self.name = name
+        self.completed = False
 
 
 
@@ -29,17 +31,22 @@ def index():
         db.session.commit() #necessary to actually add to assigned database
 
 
-    tasks= Task.query.all()
-
-    return render_template('todos.html', title="Get it Done!", tasks=tasks)
+    tasks= Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html', title="Get it Done!", tasks=tasks, 
+        completed_tasks=completed_tasks)
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
 
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
-    db.session.delete(task)
+    # db.session.delete(task)
+    # db.session.commit()
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
+
 
     return redirect('/')
 
