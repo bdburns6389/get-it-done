cef8de593ffcd002cf313f338537ffca95b34d10
Brian Burns <bdburns6389@gmail.com>
2017-12-06 07:23:15 -0600

    Completed step 6 of Lesson

--- main.py
+++ main.py
@@ -19,7 +19,51 @@ class Task(db.Model):
         self.name = name
         self.completed = False
 
+class User(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(120), unique=True)
 
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            #TODO-->
+            return redirect('/')
+        else:
+            #TODO-->
+            pass
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        #TODO Validate Data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            #TODO --> 'remember' the user
+            return redirect('/')
+        else:
+            #TODO message that says already exists in database
+            return '<h1>Duplicate User</h1>'
+
+    return render_template('register.html')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
